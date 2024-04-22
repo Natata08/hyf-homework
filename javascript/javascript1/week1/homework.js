1e28b9e6f2f62b7972d795f4ed65c8b803f2f340
Natalia Lapina <lapinann08@gmail.com>
2024-04-22 15:52:02 +0200

    add a solution for Age-ify

--- javascript/javascript1/week1/homework.js
+++ javascript/javascript1/week1/homework.js
@@ -1,2 +1,12 @@
 // Step 2
 // My freecodecamp username - @Natata08
+
+//Step 3
+// Age-ify (A future age calculator)
+
+const yearOfBirth = 1986; //number
+const yearFuture = 2025; //number
+const age = yearFuture - yearOfBirth;
+const messageAboutFututeAge = 'You will be ' + age + ' years old in ' + yearFuture;
+
+console.log(messageAboutFututeAge);
