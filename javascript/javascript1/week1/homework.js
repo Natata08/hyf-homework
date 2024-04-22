9040e1ad79238e21803f74bab80c85499b9c5d95
Natalia Lapina <lapinann08@gmail.com>
2024-04-22 16:05:18 +0200

    add a solution for Goodboy-Oldboy

--- javascript/javascript1/week1/homework.js
+++ javascript/javascript1/week1/homework.js
@@ -10,3 +10,19 @@ const age = yearFuture - yearOfBirth;
 const messageAboutFututeAge = 'You will be ' + age + ' years old in ' + yearFuture;
 
 console.log(messageAboutFututeAge);
+
+// Goodboy-Oldboy (A dog age calculator)
+
+const dogYearOfBirth = 2017;
+const dogYearFuture = 2027;
+const dogYear = dogYearFuture - dogYearOfBirth;
+const shouldShowResultInDogYears = false; //boolean
+let messageAboutDogAge;
+
+if (shouldShowResultInDogYears) {
+  messageAboutDogAge = 'Your dog will be ' + dogYear * 7 + ' dog years old in ' + dogYearFuture;
+} else {
+  messageAboutDogAge = 'Your dog will be ' + dogYear + ' human years old in ' + dogYearFuture;
+}
+
+console.log(messageAboutDogAge);
