0fe765c6fbb5efe5135dab1e6de86dc0f6cbdeee
Natalia Lapina <lapinann08@gmail.com>
2024-04-30 15:04:22 +0200

    added a gender option

--- javascript/javascript1/week2/fullname.js
+++ javascript/javascript1/week2/fullname.js
@@ -1,17 +1,25 @@
 // Flight booking fullname function
 
-function getFullname(firstname, surname, useFormalName = false) {
+function getFullname(firstname, surname, useFormalName = false, isWoman = false) {
+  const maleTitle = 'Lord';
+  const femaleTitle = 'Lady';
+
   if (firstname && surname) {
     const fullname = `${firstname} ${surname}`;
-    const fullnameFormal = useFormalName ? `${title} ${fullname}` : fullname;
-    return fullnameFormal;
+    if (!useFormalName) {
+      return fullname;
+    } else if (isWoman) {
+      return `${femaleTitle} ${fullname}`;
+    } else {
+      return `${maleTitle} ${fullname}`;
+    }
   } else {
     alert('Please fill in the missing information: First Name and Surname are required.');
   }
 }
 
-const fullname1 = getFullname('Tom', 'Jones', true);
-const fullname2 = getFullname('', 'Smith', false);
+const fullname1 = getFullname('Mary', 'Jones', true, true);
+const fullname2 = getFullname('William', 'Smith');
 
 console.log(fullname1);
 console.log(fullname2);
