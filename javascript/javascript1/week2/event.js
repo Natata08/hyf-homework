b53bbe21e8acee94ad102581f12f8be575789839
Natalia Lapina <lapinann08@gmail.com>
2024-05-01 11:32:20 +0200

    solve task Event application using remainder operator

--- javascript/javascript1/week2/event.js
+++ javascript/javascript1/week2/event.js
@@ -1,9 +1,15 @@
 // Event application
 
 function getEventWeekday(daysUntilEvent) {
-  const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const today = new Date();
-  const eventDay = new Date(today.setDate(today.getDate() + daysUntilEvent));
-  const weekDayNumber = eventDay.getDay();
-  return weekDays[weekDayNumber];
+  const todayIndex = today.getDay(); //returns the day of the week (0/sunday to 6/saturday) of a date
+  const eventDayIndex = (todayIndex + daysUntilEvent) % 7;
+  return weekdays[eventDayIndex];
 }
+
+console.log(getEventWeekday(1));
+console.log(getEventWeekday(-1));
+console.log(getEventWeekday(10));
+console.log(getEventWeekday(4));
+console.log(getEventWeekday(0));
