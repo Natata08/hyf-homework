25e8964acd0ed3e391b7c8633d6930142f7800ce
Natalia Lapina <lapinann08@gmail.com>
2024-05-02 14:53:35 +0200

    change variable's name to camelCase and checking the error check

--- javascript/javascript1/week2/fullname.js
+++ javascript/javascript1/week2/fullname.js
@@ -1,25 +1,29 @@
-// Flight booking fullname function
+// Flight booking fullName function
 
-function getFullname(firstname, surname, useFormalName = false, isWoman = false) {
-  const maleTitle = 'Lord';
-  const femaleTitle = 'Lady';
+function getFullName(firstName, surname, useFormalName = false, isWoman = false) {
+  if (
+    typeof firstName !== 'string' ||
+    typeof surname !== 'string' ||
+    firstName === '' ||
+    surname === ''
+  ) {
+    return 'Please fill in the missing information: First Name and Surname are required.';
+  } //checking that firstName and surname are not empty strings
 
-  if (firstname && surname) {
-    const fullname = `${firstname} ${surname}`;
-    if (!useFormalName) {
-      return fullname;
-    } else if (isWoman) {
-      return `${femaleTitle} ${fullname}`;
-    } else {
-      return `${maleTitle} ${fullname}`;
-    }
+  const fullName = `${firstName} ${surname}`;
+
+  if (!useFormalName) {
+    return fullName;
   } else {
-    alert('Please fill in the missing information: First Name and Surname are required.');
+    const title = isWoman ? 'Lady' : 'Lord';
+    return `${title} ${fullName}`;
   }
 }
 
-const fullname1 = getFullname('Mary', 'Jones', true, true);
-const fullname2 = getFullname('William', 'Smith');
+const fullName1 = getFullName('Mary', 'Jones', true, true);
+const fullName2 = getFullName('William', 'Smith');
+const fullName3 = getFullName('', 'Smith');
 
-console.log(fullname1);
-console.log(fullname2);
+console.log(fullName1);
+console.log(fullName2);
+console.log(fullName3);
