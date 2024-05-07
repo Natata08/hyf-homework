11882a8afa78def641abf4a1cbf3362efe4ad2ba
Natalia Lapina <lapinann08@gmail.com>
2024-05-07 14:05:25 +0200

    add solution for travel time

---
+++ javascript/javascript1/week3/travel-time.js
@@ -0,0 +1,35 @@
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+
+const travelInformation1 = {
+  speed: 50,
+  destinationDistance: 38,
+};
+
+function formatTimeInString(hours, minutes) {
+  const hourText = hours === 1 ? 'hour' : 'hours';
+  const minuteText = minutes === 1 ? 'minute' : 'minutes';
+  let output = '';
+  if (hours !== 0 || minutes === 0) {
+    output += `${hours} ${hourText}`;
+  }
+  if (minutes > 0) {
+    if (hours > 0) output += ' and ';
+    output += `${minutes} ${minuteText}`;
+  }
+  return output;
+}
+
+function calculateTravelTime(travelInformation) {
+  const travelTime = travelInformation.destinationDistance / travelInformation.speed;
+  const travelTimeHours = Math.floor(travelTime);
+  const travelTimeMinutes = Math.round((travelTime - travelTimeHours) * 60);
+  return formatTimeInString(travelTimeHours, travelTimeMinutes);
+}
+
+const travelTime = calculateTravelTime(travelInformation);
+console.log(travelTime); // 8 hours and 38 minutes
+const travelTime1 = calculateTravelTime(travelInformation1);
+console.log(travelTime1); // 46 minutes
