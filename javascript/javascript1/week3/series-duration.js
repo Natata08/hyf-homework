e74972b40784f5f05661337eeb440e489f18c6cc
Natalia Lapina <lapinann08@gmail.com>
2024-05-08 11:41:41 +0200

    add data for Series duration of my life

---
+++ javascript/javascript1/week3/series-duration.js
@@ -0,0 +1,28 @@
+const seriesDurations = [
+  {
+    title: 'Breaking Bad',
+    days: 2,
+    hours: 3,
+    minutes: 40,
+  },
+  {
+    title: 'Scrubs',
+    days: 3,
+    hours: 0,
+    minutes: 48,
+  },
+  {
+    title: 'Game of Thrones',
+    days: 2,
+    hours: 20,
+    minutes: 8,
+  },
+  {
+    title: 'House M.D.',
+    days: 5,
+    hours: 9,
+    minutes: 48,
+  },
+];
+
+const averageLifespanInYears = 80;
