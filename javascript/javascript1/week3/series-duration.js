16e5745ec482f4e9e5708accca7dd2ad43a16daa
Natalia Lapina <lapinann08@gmail.com>
2024-05-08 12:44:41 +0200

    add solution for Series duration of my life

--- javascript/javascript1/week3/series-duration.js
+++ javascript/javascript1/week3/series-duration.js
@@ -26,3 +26,51 @@ const seriesDurations = [
 ];
 
 const averageLifespanInYears = 80;
+
+function yearsToMinutes(years) {
+  const minutesPerYear = 525600; // 365 days * 24 hours * 60 minutes
+  return years * minutesPerYear;
+}
+
+const calculatePercentageOfLifespan = (durationInMin) =>
+  Number(((durationInMin / yearsToMinutes(averageLifespanInYears)) * 100).toFixed(3));
+
+function calculateSeriesPercentage(seriesInformation) {
+  let totalDurationInMin = 0;
+  const percentages = [];
+  for (let i = 0; i < seriesInformation.length; i++) {
+    const series = seriesInformation[i];
+    const durationInMin = series.days * 24 * 60 + series.hours * 60 + series.minutes;
+    totalDurationInMin += durationInMin;
+    const percentagePerSeries = calculatePercentageOfLifespan(durationInMin);
+    percentages.push([series.title, percentagePerSeries]);
+  }
+  const totalPercentage = calculatePercentageOfLifespan(totalDurationInMin);
+  percentages.push(totalPercentage);
+
+  return percentages; //looks like [[str, num], [str, num], [str, num], num]
+}
+
+function logOutSeriesText() {
+  const percentageInformation = calculateSeriesPercentage(seriesDurations);
+  let output = '';
+  for (let i = 0; i < percentageInformation.length; i++) {
+    if (i === percentageInformation.length - 1) {
+      output += '\n' + `In total that is ${percentageInformation[i]}% of my life`;
+    } else {
+      output +=
+        `${percentageInformation[i][0]} took ${percentageInformation[i][1]}% of my life` + '\n';
+    }
+  }
+  console.log(output);
+}
+
+logOutSeriesText();
+
+// Output:
+// Breaking Bad took 0.007% of my life
+// Scrubs took 0.01% of my life
+// Game of Thrones took 0.01% of my life
+// House M.D. took 0.019% of my life
+
+// In total that is 0.046% of my life
