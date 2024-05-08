bc0324b9cf9aa5b87ea0ccf671163978d1392ff8
Natalia Lapina <lapinann08@gmail.com>
2024-05-08 14:17:49 +0200

    add functionality to get a note

--- javascript/javascript1/week3/notepad.js
+++ javascript/javascript1/week3/notepad.js
@@ -10,3 +10,24 @@ saveNote('Pick up groceries', 1);
 saveNote('Do laundry', 2);
 
 console.log(notes); //[{ content: 'Pick up groceries', id: 1 }, { content: 'Do laundry', id: 2 }]
+
+//get a note
+function getNote(id) {
+  if (!id || typeof id !== 'number') {
+    return 'Error: The id is not specified or the id is not a number';
+  }
+  for (let i = 0; i < notes.length; i++) {
+    const note = notes[i];
+    if (note.id === id) return note;
+  }
+  return 'A note with such an id does not exist';
+}
+
+const firstNote = getNote(1);
+console.log(firstNote); // {content: 'Pick up groceries', id: 1}
+const firstNote1 = getNote();
+console.log(firstNote1); // Error: The id is not specified or the id is not a number
+const firstNote2 = getNote('2');
+console.log(firstNote2); // Error: The id is not specified or the id is not a number
+const firstNote3 = getNote(3);
+console.log(firstNote3); // A note with such an id does not exist
