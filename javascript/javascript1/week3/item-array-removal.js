c6e432e28781a14b925cdfd88535562f78c99ecf
Natalia Lapina <lapinann08@gmail.com>
2024-05-12 09:14:44 +0200

    move logic to function, add additional check

--- javascript/javascript1/week3/item-array-removal.js
+++ javascript/javascript1/week3/item-array-removal.js
@@ -10,7 +10,17 @@ const names = [
 ];
 const nameToRemove = 'Ahmad';
 
-const indexOfRemovingName = names.indexOf(nameToRemove);
-names.splice(indexOfRemovingName, 1);
+function removeName(namesList, nameToRemove) {
+  const indexOfRemovingName = namesList.indexOf(nameToRemove);
+  if (indexOfRemovingName >= 0) {
+    namesList.splice(indexOfRemovingName, 1);
+  } else {
+    console.log(`Error: The name '${nameToRemove}' was not found in the list.`);
+  }
+}
 
+removeName(names, nameToRemove);
+console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+
+removeName(names, 'Sam'); //Error: The name 'Sam' was not found in the list.
 console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
