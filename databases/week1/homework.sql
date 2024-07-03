a0b673fb3f9c96501715aa52513201fbbc473024
Natalia Lapina <lapinann08@gmail.com>
2024-07-03 15:52:11 +0200

    add query #4

--- databases/week1/homework.sql
+++ databases/week1/homework.sql
@@ -12,32 +12,35 @@ WHERE
 -- 3. Find all the tasks that are marked as done
 SELECT * FROM task WHERE status_id = 3;
 
--- 4. Get all the tasks, sorted with the most recently created first
+-- 4. Find all the tasks that are not marked as done
+SELECT * FROM task WHERE NOT status_id = 3;
+
+-- 5. Get all the tasks, sorted with the most recently created first
 SELECT * FROM task ORDER BY created DESC;
 
--- 5. Get the single most recently created task
+-- 6. Get the single most recently created task
 SELECT * FROM task ORDER BY created DESC LIMIT 1;
 
--- 6. Get the title and due date of all tasks where the title or description contains database
+-- 7. Get the title and due date of all tasks where the title or description contains database
 SELECT title, due_date
 FROM task
 WHERE
     title LIKE '%database%'
     OR description LIKE '%database%';
 
--- 7. Get the title and status (as text) of all tasks
+-- 8. Get the title and status (as text) of all tasks
 SELECT task.title, STATUS.name
 FROM task
     LEFT JOIN STATUS ON task.status_id = STATUS.id;
 
--- 8. Get the name of each status, along with a count of how many tasks have that status
+-- 9. Get the name of each status, along with a count of how many tasks have that status
 SELECT STATUS.name AS status_name, COUNT(*) AS task_count
 FROM task
     LEFT JOIN STATUS ON task.status_id = STATUS.id
 GROUP BY
     STATUS.name;
 
--- 9. Get the names of all statuses, sorted by the status with most tasks first
+-- 10. Get the names of all statuses, sorted by the status with most tasks first
 SELECT STATUS.name AS status_name, COUNT(*) AS task_count
 FROM task
     LEFT JOIN STATUS ON task.status_id = STATUS.id
