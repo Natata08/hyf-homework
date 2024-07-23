b3404831283cf9e07f4837f67467e7279407ce95
Natalia Lapina <lapinann08@gmail.com>
2024-07-23 14:42:24 +0200

    fix a naming error

--- databases/week3/meal-sharing-db.sql
+++ databases/week3/meal-sharing-db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE your_database_name CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE meal_sharing_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE `meal` (
     `id` int AUTO_INCREMENT PRIMARY KEY,
