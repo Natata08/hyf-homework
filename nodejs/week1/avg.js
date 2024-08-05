4b989ca91b4008b3b1540a3c1fac7d39b6cf95bc
Natalia Lapina <lapinann08@gmail.com>
2024-08-05 14:48:41 +0200

    add warmup task

---
+++ nodejs/week1/avg.js
@@ -0,0 +1,49 @@
+const arguments = process.argv.slice(2);
+
+const parseArguments = (arguments) => {
+  return arguments.reduce(
+    (acc, arg) => {
+      const num = parseFloat(arg);
+      if (!isNaN(num)) {
+        acc.validArguments.push(num);
+      } else {
+        acc.invalidArguments.push(arg);
+      }
+      return acc;
+    },
+    { validArguments: [], invalidArguments: [] }
+  );
+};
+
+const calculateAverage = (numbers) =>
+  numbers.reduce((acc, num) => acc + num, 0) / numbers.length;
+
+const produceAverageOutput = (validArguments, invalidArguments) => {
+  if (validArguments.length === 0) {
+    return 'No numbers provided';
+  }
+  if (invalidArguments.length !== 0) {
+    console.log(
+      `Warning: The following arguments are not valid numbers and will be ignored: ${invalidArguments.join(
+        ', '
+      )}`
+    );
+  }
+  return calculateAverage(validArguments);
+};
+
+const { validArguments, invalidArguments } = parseArguments(arguments);
+console.log(produceAverageOutput(validArguments, invalidArguments));
+
+//node avg.js 13 7 4
+//Output:
+//8
+
+//node avg.js 1 two hello 4
+//Output:
+//Warning: The following arguments are not valid numbers and will be ignored: two, hello
+//2.5
+
+//node avg.js
+//Output:
+//No numbers provided
